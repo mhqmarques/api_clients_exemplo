cf236743bfa45260a356d8e2cc4d71589749c0e6
Marlon Henrique Queroga Marques <mhqmarques@gmail.com>
2020-09-25 11:20:41 -0300

    adicionado autenticacao e atorizacao via JWT

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 const express = require('express')
 const app = express()
 const data = require("./data.json")
+require("dotenv-safe").config()
+let jwt = require("jsonwebtoken")
 
 app.use(express.json())
 
 //GET: Receber dados de um Resource
-app.get('/clients', function (req, res) {
+app.get('/clients', verifyJWT, function (req, res, next) {
     res.json(data)
 })
-app.get('/clients/:id', function (req, res) {
+app.get('/clients/:id', verifyJWT, function (req, res, next) {
     const {id} = req.params
     const client = data.find(client => client.id == id)
 
@@ -18,7 +20,7 @@ app.get('/clients/:id', function (req, res) {
     res.json(client)
 })
 //POST: Enviar dados ou informações para serem processados por um Resource
-app.post('/clients', function (req, res) {
+app.post('/clients', verifyJWT, function (req, res, next) {
     const {name, username, email} = req.body
     let id = data[data.length-1].id
     ++id
@@ -29,7 +31,7 @@ app.post('/clients', function (req, res) {
 
 })
 //PUT: Atualiza dados de um Resource
-app.put('/clients/:id', function (req, res) {
+app.put('/clients/:id', verifyJWT, function (req, res, next) {
     const {id} = req.params
     const client = data.find(client => client.id == id)
 
@@ -45,7 +47,7 @@ app.put('/clients/:id', function (req, res) {
     res.json(client)
 })
 //DELETE: Delatar Resource
-app.delete('/clients/:id', function (req, res) {
+app.delete('/clients/:id', verifyJWT, function (req, res, next) {
     const {id} = req.params
     const client = data.find(client => client.id == id)
     const clientsFiltered = data.filter(client => client.id != id)
@@ -55,9 +57,37 @@ app.delete('/clients/:id', function (req, res) {
     }
 
     res.json(clientsFiltered)
-
-
 })
+
+//autenticação
+app.post('/login', (req, res, next) => {
+    // simulação de autenticação no banco de dados
+    if(req.body.user === 'Marlon' && req.body.pwd === '123') {
+        //auth ok
+        const id = 1; // id que retornaria do banco de dados
+        let token = jwt.sign({id}, process.env.SECRET, {
+            expiresIn: 300 // expira em 5 minutos
+        })
+        console.log('Login realizado e token gerado com sucesso!!');
+        
+        return res.json({auth: true, token: token})
+    }
+    res.status(500).json({message: 'Login inválido'})
+})
+
+//autorização
+    function verifyJWT(req, res, next){
+        let token = req.headers['x-access-token'];
+        if (!token) return res.status(401).json({ auth: false, message: 'No token provided.' });
+        
+        jwt.verify(token, process.env.SECRET, function(err, decoded) {
+        if (err) return res.status(500).json({ auth: false, message: 'Failed to authenticate token.' });
+        
+        // se tudo estiver ok, salva no request para uso posterior
+        req.userId = decoded.id;
+        next();
+        });
+    }
 
 
 app.listen(3000, function () {
